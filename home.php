672ca4a3f4927bfa8e465b42265d01e10ff12d1a
GitHugop <ori.ginalhugop@gmail.com>
2017-04-14 09:50:02 +0200

    Update home.php

--- home.php
+++ home.php
@@ -1,4 +1,56 @@
+<html>
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+<h1>Welcome</h1>
+<p>You have been successfully logged in.</p>
+<p>
+    <a href="logout.php">Log Out</a><br> <a href="change.php">Change Password</a><br> <a href="adm/adminpanel.php">Admin
+        Panel</a>
 
+</p>
+<p>
+    You registered on this website <strong>1 Month</strong> ago.
+</p>
+<p>
+</p>
+<pre></pre>
+<p>
+    Change display name:
+</p>
+<form action="home.php" method="POST">
+    <input name="newName" placeholder="New name">
+    <button>Change Name</button>
+</form>
+Post something:
+<form action="home.php" method="POST">
+    <input name="postMessage" placeholder="Hmm...">
+    <button>Post</button>
+</form>
+<p>
+</p>
+---
 
-<html> <head>     <meta charset="utf-8"> </head> <body> <h1>Welcome</h1> <p>You have been successfully logged in.</p> <p>     <a href="logout.php">Log Out</a><br> <a href="change.php">Change Password</a><br> <a href="adm/adminpanel.php">Admin         Panel</a> </p> <p>     You registered on this website <strong>1 Month</strong> ago. </p> <p> </p> <pre></pre> <p>     Change display name: </p> <form action="home.php" method="POST">     <input name="newName" placeholder="New name">     <button>Change Name</button> </form> Post something: <form action="home.php" method="POST">     <input name="postMessage" placeholder="Hmm...">     <button>Post</button> </form> <p> </p> --- <div class="posts">     <strong>&amp;#128515;</strong> 2017-03-18 13:21:39<br>     @hugo ---<br>     <div class="post">&amp;#128515;</div>     <br>     <hr> </div> <div class="posts">     <strong>&amp;#128515;</strong> 2017-03-18 13:20:43<br>     @hugo ---<br>     <div class="post">&amp;#128515;</div>     <br>     <hr> </div> <div class="posts">     <strong>&amp;#128515;</strong> 2017-03-18 13:20:15<br>     @hugo ---<br>     <div class="post">😃</div>     <br>     <hr> </div> <div class="posts">     <strong>&amp;#128515;</strong> 2017-03-18 13:20:11<br>     @hugo ---<br>     <div class="post">😃</div>     <br>     <hr> </div> <div class="posts">     <strong>&amp;#128515;</strong> 2017-03-18 13:19:23<br>     @hugo ---<br>     <div class="post">????</div>     <br>     <hr> </div> <div class="posts">     <strong>&amp;#128515;</strong> 2017-03-18 13:10:36<br>     @hugo ---<br>     <div class="post">????</div>     <br>     <hr> </div> <div class="posts">     <strong>&amp;#128515;</strong> 2017-03-18 13:09:07<br>     @hugo ---<br>     <div class="post">????</div>     <br>     <hr> </div> <div class="posts">     <strong>&amp;#128515;</strong> 2017-03-18 13:05:45<br>     @hugo ---<br>     <div class="post">????</div>     <br>     <hr> </div> <div class="posts">     <strong>&amp;#128515;</strong> 2017-03-18 13:04:27<br>     @hugo ---<br>     <div class="post">????</div>     <br>     <hr> </div> <div class="posts">     <strong>&amp;#128515;</strong> 2017-03-18 13:04:19<br>     @hugo ---<br>     <div class="post">&amp;#128515;</div>     <br>     <hr> </div> <div class="posts">     <strong>&amp;#128515;</strong> 2017-03-18 13:03:50<br>     @hugo ---<br>     <div class="post">&amp;#128515;</div>     <br>     <hr> </div> <div class="posts">     <strong>&amp;#128515;</strong> 2017-03-18 13:02:57<br>     @hugo ---<br>     <div class="post">????</div>     <br>     <hr> </div> <div class="posts">     <strong>&amp;#128515;</strong> 2017-03-18 13:00:30<br>     @hugo ---<br>     <div class="post">????</div>     <br>     <hr> </div> <div class="posts">     <strong>&amp;#128515;</strong> 2017-03-18 12:52:42<br>     @hugo ---<br>     <div class="post">????</div>     <br>     <hr> </div> <div class="posts">     <strong>&amp;#128515;</strong> 2017-03-18 12:52:02<br>     @hugo ---<br>     <div class="post">????</div>     <br>     <hr> </div> <div class="posts">     <strong>&amp;#128515;</strong> 2017-03-18 12:45:48<br>     @hugo ---<br>     <div class="post">&lt;?</div>     <br>     <hr> </div> <div class="posts">     <strong>&amp;#128515;</strong> 2017-03-18 12:45:41<br>     @hugo ---<br>     <div class="post">test</div>     <br>     <hr> </div> <div class="posts">     <strong>&amp;#128515;</strong> 2017-03-18 12:45:32<br>     @hugo ---<br>     <div class="post">????</div>     <br>     <hr> </div> <div class="posts">     <strong>&amp;#128515;</strong> 2017-03-18 12:40:23<br>     @hugo ---<br>     <div class="post">&amp;#128515;</div>     <br>     <hr> </div> <div class="posts">     <strong>&amp;#128515;</strong> 2017-03-18 12:40:02<br>     @hugo ---<br>     <div class="post">&amp;#128515;</div>     <br>     <hr> </div> <div class="posts">     <strong>&amp;#128515;</strong> 2017-03-17 16:57:25<br>     @hugo ---<br>     <div class="post">&amp;#129300;</div>     <br>     <hr> </div> <div class="posts">     <strong>&amp;#128515;</strong> 2017-03-17 04:54:34<br>     @hugo ---<br>     <div class="post">😃</div>     <br>     <hr> </div> <div class="posts">     <strong>&amp;#128515;</strong> 2017-03-17 04:51:11<br>     @hugo ---<br>     <div class="post">&amp;#128064;</div>     <br>     <hr> </div> <div class="posts">     <strong>&amp;#128515;</strong> 2017-03-17 04:50:34<br>     @hugo ---<br>     <div class="post">Yup :D</div>     <br>     <hr> </div> <div class="posts">     <strong>&amp;#128515;</strong> 2017-03-17 04:50:24<br>     @hugo ---<br>     <div class="post">Yup :D</div>     <br>     <hr> </div> <div class="posts">     <strong>&amp;#128515;</strong> 2017-03-17 04:50:14<br>     @hugo ---<br>     <div class="post">This should work now</div>     <br>     <hr> </div> <div class="posts">     <strong>&amp;#128515;</strong> 2017-03-17 04:42:46<br>     @hugo ---<br>     <div class="post">&lt;h3&gt;Test</div>     <br>     <hr> </div> <div class="posts">     <strong>&amp;#128515;</strong> 2017-03-16 05:47:01<br>     @hugo ---<br>     <div class="post">Test</div>     <br>     <hr> </div> <div class="posts">     <strong>&amp;#128515;</strong> 2017-03-14 17:54:23<br>     @hugo ---<br>     <div class="post">Hugo uses Masterball!</div>     <br>     <hr> </div> <div class="posts">     <strong>Birdy The Phoenix</strong> 2017-03-14 14:11:03<br>     @birdythephoenix ---<br>     <div class="post">A wild birb has appeared.</div>     <br>     <hr> </div> <div class="posts">     <strong>&amp;#128515;</strong> 2017-03-14 14:10:56<br>     @hugo ---<br>     <div class="post">&lt;3 ~</div>     <br>     <hr> </div> <div class="posts">     <strong>&amp;#128515;</strong> 2017-03-14 14:10:45<br>     @hugo ---<br>     <div class="post">Hii</div>     <br>     <hr> </div> <div class="posts">     <strong>GuGu xxXxx</strong> 2017-03-13 04:57:57<br>     @gugu ---<br>     <div class="post">Testing</div>     <br>     <hr> </div> <div class="posts">     <strong>&amp;#128515;</strong> 2017-03-12 18:03:01<br>     @hugo ---<br>     <div class="post">Argh some letters don't work</div>     <br>     <hr> </div> <div class="posts">     <strong>&amp;#128515;</strong> 2017-03-12 18:02:53<br>     @hugo ---<br>     <div class="post">z</div>     <br>     <hr> </div> <div class="posts">     <strong>&amp;#128515;</strong> 2017-03-12 18:02:48<br>     @hugo ---<br>     <div class="post">ze</div>     <br>     <hr> </div> <div class="posts">     <strong>&amp;#128515;</strong> 2017-03-12 18:02:38<br>     @hugo ---<br>     <div class="post">Hmm, will have to disable empty ones</div>     <br>     <hr> </div> <div class="posts">     <strong>&amp;#128515;</strong> 2017-03-12 18:02:16<br>     @hugo ---<br>     <div class="post"></div>     <br>     <hr> </div> <div class="posts">     <strong>&amp;#128515;</strong> 2017-03-12 18:02:14<br>     @hugo ---<br>     <div class="post"></div>     <br>     <hr> </div> <div class="posts">     <strong>&amp;#128515;</strong> 2017-03-12 17:53:08<br>     @hugo ---<br>     <div class="post">Yus, it works :D</div>     <br>     <hr> </div> <div class="posts">     <strong>&amp;#128515;</strong> 2017-03-12 14:23:02<br>     @hugo ---<br>     <div class="post">Hmm</div>     <br>     <hr> </div> <div class="posts">     <strong>&amp;#128515;</strong> 2017-03-12 13:33:21<br>     @hugo ---<br>     <div class="post">Does this work? Hmm...</div>     <br>     <hr> </div> </body> </html>
+  <div class="posts">
 
+    <strong>Hugo</strong> 2017-03-18 13:21:39<br>
+    @hugo ---<br>
+    <div class="post">This is a test post.</div>
+    <br>
+    <hr>
+
+</div>
+  
+<div class="posts">
+
+    <strong>Hugo</strong> 2017-03-18 13:21:39<br>
+    @hugo ---<br>
+    <div class="post">&amp;#128515;</div>
+    <br>
+    <hr>
+
+</div>
+
+</body>
+</html>
