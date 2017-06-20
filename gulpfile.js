65d127ba599479d1753b09b7100ecd9a88ab5646
Lapoozza <jewelmahanta@gmail.com>
2017-06-20 21:31:35 +0530

    chore (build sass): added auto-prefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 var gulp = require('gulp')
 var sass = require('gulp-sass')
+var autoprefixer = require('gulp-autoprefixer')
 
 // Compile sass to regular css
 gulp.task('sass', function () {
   return gulp.src('assets/sass/**/*.sass')
       .pipe(sass())
+      .pipe(autoprefixer({
+        cascade: false
+      }))
       .pipe(gulp.dest('assets/css'));
 });
